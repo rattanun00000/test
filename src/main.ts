b8fab8c58206aad183acf62e400a3bd8398a4d76
rattanun00000 <rattanun901@gmail.com>
2025-02-07 22:35:01 +0700

    testac

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+  
   const allowedOrigins = configService.get<string>('ALLOWED_ORIGINS')?.split(',') ?? [];
-
+  
   app.enableCors({
-    origin: allowedOrigins, // กำหนด domain ที่อนุญาตฟหก
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // กำหนด HTTP methods ที่อนุญาต
-    credentials: true, // อนุญาตให้ส่ง credentials (cookies, headers)
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 8000);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
