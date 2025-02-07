f9a11bf6bcea9ba6eae263fd45bfe5df24195715
rattanun00000 <rattanun901@gmail.com>
2025-02-08 01:39:21 +0700

    aaa

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,16 @@ async function bootstrap() {
 
   app.enableCors({
     origin: allowedOrigins, // กำหนด domain ที่อนุญาตฟหก
-    methods: ['GET'], // กำหนด HTTP methods ที่อนุญาต
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // กำหนด HTTP methods ที่อนุญาต
     credentials: true, // อนุญาตให้ส่ง credentials (cookies, headers)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
   });
 
   await app.listen(process.env.PORT ?? 8000);
